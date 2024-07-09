2b56f3a411a51f408ec2b648af4ae2ef82969162
LoicSteve <loikstevegmail.com>
2024-07-09 15:55:49 +0200

    first test

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+def add(x, y):
+    return int(x + y)
+
+
+print(add(1, 1))
+
+var = 1
+var = var
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from main import add
+
+
+def test_add():
+    assert 9 == add(5, 4)
